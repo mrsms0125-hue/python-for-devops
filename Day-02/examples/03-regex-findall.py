c31101207739c19d094f2e026cbc40d03da8a0fa
mrsms0125-hue <mrsms0125@gmail.com>
2025-11-07 00:20:53 +0530

    Enhance regex examples in 03-regex-findall.py
    Add regex matching examples for re.match, re.search, and re.findall.

--- Day-02/examples/03-regex-findall.py
+++ Day-02/examples/03-regex-findall.py
@@ -8,3 +8,31 @@ if search:
     print("Pattern found:", search.group())
 else:
     print("Pattern not found")
+
+
+
+
+
+
+
+
+import re
+
+text="AND1USER 1, USER 2 AND USER 3 HAVE LOG ON ANDAND2"
+
+pattern=r"AND\d"
+match1=re.match(pattern,text)
+if match1:
+    print(match1.group())
+else:
+    print("no match1")
+
+match2=re.search(pattern,text)
+if match2:
+    print(match2.group())
+else:
+    print("no match2")
+
+match3=re.findall(pattern,text)
+print(match3[1])
+
